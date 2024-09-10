130617c7dcea6802e7ab724e034030f480b475c0
MaDLuck5 <matsslik5@gmail.com>
2024-09-10 17:00:47 +0200

    added PrimerObject to store and get the primers from the start page

--- src/main/java/nl/mats/primer_evaluation_tool/PrimerObject.java
+++ src/main/java/nl/mats/primer_evaluation_tool/PrimerObject.java
@@ -1,4 +1,36 @@
 package nl.mats.primer_evaluation_tool;
 
 public class PrimerObject {
+    private String primer1;
+    private String primer2;
+
+    public PrimerObject() {}
+
+    // constructors
+
+    public PrimerObject(String primer1, String primer2) {
+        this.primer1 = primer1;
+        this.primer2 = primer2;
+    }
+
+    // getters and setters
+    public String getPrimer1() {
+        return primer1;
+    }
+    public void setPrimer1(String primer1) {
+        this.primer1 = primer1;
+    }
+
+    public String getPrimer2() {
+        return primer2;
+    }
+    public void setPrimer2(String primer2) {
+        this.primer2 = primer2;
+    }
+
+    // to string methode
+    public String toString() {
+        return primer1 + " " + primer2;
+    }
+
 }
