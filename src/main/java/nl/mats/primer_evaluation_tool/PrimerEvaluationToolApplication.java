d7bec88a48864213dcce8482044d11f313e3019b
MaDLuck5 <matsslik5@gmail.com>
2024-09-10 10:01:12 +0200

    first commit

---
+++ src/main/java/nl/mats/primer_evaluation_tool/PrimerEvaluationToolApplication.java
@@ -0,0 +1,17 @@
+package nl.mats.primer_evaluation_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerEvaluationToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimerEvaluationToolApplication.class, args);
+    }
+
+    public static void control(String [] args) {
+
+    }
+
+}
---
+++ src/test/java/nl/mats/primer_evaluation_tool/PrimerEvaluationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mats.primer_evaluation_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerEvaluationToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
