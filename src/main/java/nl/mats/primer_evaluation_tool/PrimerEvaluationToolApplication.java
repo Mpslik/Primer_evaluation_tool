9d40302ee41875b391f7c5e524f1dd48782fe558
MaDLuck5 <matsslik5@gmail.com>
2024-09-11 09:39:47 +0200

    Main class

--- src/main/java/nl/mats/primer_evaluation_tool/PrimerEvaluationToolApplication.java
+++ src/main/java/nl/mats/primer_evaluation_tool/PrimerEvaluationToolApplication.java
@@ -1,25 +1,12 @@
 package nl.mats.primer_evaluation_tool;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import javax.annotation.processing.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Controller
+@SpringBootApplication
 public class PrimerEvaluationToolApplication {
 
-    @Autowired
-    private AnalysesHistory analysesHistory;
-
-    @GetMapping("/")
-    public String showForm(Model model) {
-    model.addAttribute("primerForm", new PrimerObject());
-
-    model.addAttribute("history", analysesHistory.getHistory());
-
-    return "start_page";
+    public static void main(String[] args) {
+        SpringApplication.run(PrimerEvaluationToolApplication.class, args);
     }
 }
